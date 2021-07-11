2257dbec4a4fe9c653f95dcbf6443c2521bac29d
a2athoth <blockchain.azathoth@gmail.com>
2021-07-12 00:01:14 +0900

    todo Checked 기능

--- js/todo.js
+++ js/todo.js
@@ -15,6 +15,28 @@ function deleteTodo(event) {
     saveTodos(todos);
 }
 
+function editTodo(event) {
+    const li = event.target.parentElement;
+    const checkbox = li.querySelector("input[type=checkbox]");
+
+    console.log(checkbox.checked);
+
+    console.log("todosb4", todos);
+    todos.forEach((item) => {
+        if (item.id === parseInt(li.id)) {
+            item.checked = checkbox.checked;
+            console.log("changed");
+        }
+    });
+    console.log("todosAft", todos);
+
+    // todos = todos.filter((item) => {
+    //   return item.id !== parseInt(li.id);
+    // });
+
+    saveTodos(todos);
+}
+
 function saveTodos(todoList) {
     localStorage.setItem(TODOS_KEY, JSON.stringify(todoList));
     if (todoList.length === 0) {
@@ -26,24 +48,35 @@ function drawTodo(newTodo) {
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
+    const checkbox = document.createElement("input");
     const button = document.createElement("button");
+    checkbox.setAttribute("type", "checkbox");
     button.innerText = "X";
+    checkbox.addEventListener("change", editTodo);
     button.addEventListener("click", deleteTodo);
 
+    li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(button);
 
     span.innerText = newTodo.text;
+    if (newTodo.checked) {
+        checkbox.setAttribute("checked", true);
+    }
     todoList.appendChild(li);
 }
 
 function handleTodoSubmit(event) {
     event.preventDefault();
-    const newTodo = { id: Date.now(), text: todoInput.value };
+    const newTodo = { id: Date.now(), text: todoInput.value, checked: false };
     todoInput.value = ""; //clear input
     todos.push(newTodo);
     drawTodo(newTodo);
     saveTodos(todos);
+}
+
+function handleTodoSelected(event) {
+    event.preventDefault();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
